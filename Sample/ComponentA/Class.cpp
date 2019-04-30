f0d4bc247c426bc8a29cf3ea77920e9a69ec7d5c
Kenny Kerr <kekerr@microsoft.com>
2019-04-30 14:59:54 -0700

    Sample

--- Sample/ComponentA/Class.cpp
+++ Sample/ComponentA/Class.cpp
@@ -2,10 +2,14 @@
 #include "Class.h"
 #include "Class.g.cpp"
 
+using namespace std::literals;
+
 namespace winrt::ComponentA::implementation
 {
     Windows::Foundation::IAsyncOperation<hstring> Class::GetNameAsync() const
     {
+        co_await 1000ms;
+
         co_return L"ComponentA";
     }
 }
--- Sample/ComponentB/Class.cpp
+++ Sample/ComponentB/Class.cpp
@@ -2,10 +2,14 @@
 #include "Class.h"
 #include "Class.g.cpp"
 
+using namespace std::literals;
+
 namespace winrt::ComponentB::implementation
 {
     Windows::Foundation::IAsyncOperation<hstring> Class::GetNameAsync() const
     {
+        co_await 1000ms;
+
         co_return L"ComponentB";
     }
 }
